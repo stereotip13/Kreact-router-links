c5560ca30cd815ab69dec96bd4441ab5dc34df64
stereotip13 <stereotip13@gmail.com>
2023-03-08 22:38:26 +0500

    next step

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,11 +1,19 @@
 //import { useState, useEffect } from 'react';
-import { Link, useLoaderData, useSearchParams } from 'react-router-dom';
+import { Suspense } from 'react';
+import {
+  Await,
+  defer,
+  Link,
+  useLoaderData,
+  useSearchParams,
+} from 'react-router-dom';
 import { BlogFilter } from '../components/BlogFilter';
 
 const Blogpage = () => {
   //const [posts, setPosts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const posts = useLoaderData(); //хук который достает посты из лоадера
+  //posts - в фигурных скобках это уже получается объект как бы мы типизируем
+  const { posts } = useLoaderData(); //хук который достает посты из лоадера
   const postQuery = searchParams.get('post') || '';
   const latest = searchParams.has('latest');
 
@@ -32,23 +40,41 @@ const Blogpage = () => {
       >
         Add new post
       </Link>
-      {posts
-        .filter(
-          (post) => post.title.includes(postQuery) && post.id >= startsFrom
-        )
-        // ниже пробегаемся map по постам в стейте, для каждого поста post делать
-        // отрисовку
-        .map((post) => (
-          <Link key={post.id} to={`/posts/${post.id}`}>
-            <li>{post.title}</li>
-          </Link>
-        ))}
+      {/* Саспенс компонент позволяющий ждать загрузки кода стр */}
+      <Suspense fallback={<h1>LOADING....</h1>}>
+        {/* ПОка не загрузятся посты будет показывать лоадинг */}
+        <Await resolve={posts}>
+          {/* далее делаем рендер пропс , НАЗВАНИЕ ПРОПСОВ ДЕЛАЕМ ЛЮБОЕ*/}
+          {(resolvedPosts) => (
+            <>
+              {resolvedPosts
+                .filter(
+                  (post) =>
+                    post.title.includes(postQuery) && post.id >= startsFrom
+                )
+                // ниже пробегаемся map по постам в стейте, для каждого поста post делать
+                // отрисовку
+                .map((post) => (
+                  <Link key={post.id} to={`/posts/${post.id}`}>
+                    <li>{post.title}</li>
+                  </Link>
+                ))}
+            </>
+          )}
+        </Await>
+      </Suspense>
     </div>
   );
 };
-const blogloader = async ({ request, params }) => {
-  console.log({ request, params });
+async function getPosts() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts'); // делает запрос
-  return res.json(); // разбирает данные
+  return res.json(); // разбирает данные}
+}
+const blogloader = async () => {
+  //За счет фунции defer у нас есть возможность ожидать часть данных которые будут загружены на страницу
+  return defer({
+    posts: getPosts(),
+  });
 };
+
 export { Blogpage, blogloader };
--- src/pages/Singlepage.jsx
+++ src/pages/Singlepage.jsx
@@ -1,8 +1,39 @@
-import { useParams, Link, useNavigate, useLoaderData } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import {
+  useParams,
+  Link,
+  useNavigate,
+  useLoaderData,
+  Await,
+  useAsyncValue,
+} from 'react-router-dom';
+import { useState, useEffect, Suspense } from 'react';
 
+const Post = () => {
+  const post = useAsyncValue();
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
+};
+const Comments = () => {
+  const comments = useAsyncValue();
+  return (
+    <div>
+      <h2>Comments</h2>
+      {comments.map((comment) => (
+        <>
+          <h3>{comment.email}</h3>
+          <h4>{comment.name}</h4>
+          <p>{comment.body}</p>
+        </>
+      ))}
+    </div>
+  );
+};
 const Singlepage = () => {
-  const { post, id } = useLoaderData(); // с помощью этого хука забираем информацию из роутов
+  const { post, id, comments } = useLoaderData(); // с помощью этого хука забираем информацию из роутов
   //const { id } = useParams(); //ключ объекта(параметр) носит такое же имя как в роутах, только теперь их можно использовать
   const navigate = useNavigate(); //использование функции для навигации
   //const [post, setPost] = useState(); //сохраняем массив в стайте
@@ -23,10 +54,19 @@ const Singlepage = () => {
       <button onClick={goBack}>Go back</button>
       {/* кнопка домой ниже привеженным образом не лучшая практика, т.к. лучше восп-ся link */}
       <button onClick={goHome}>Go home</button>
+      <Suspense fallback={<h2>loading post ...</h2>}>
+        <Await resolve={post}>
+          <Post />
+        </Await>
+      </Suspense>
+      {/* Саспенс позволяет показывать сообщение пока мы ждем загрузку */}
+      <Suspense fallback={<h2>comment is loading...</h2>}>
+        <Await resolve={comments}>
+          <Comments />
+        </Await>
+      </Suspense>
       {post && (
         <>
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
           <Link to={`/posts/${id}/edit`}>
             Редактровать текущий пост с текущим айдишником : {id}
           </Link>
@@ -35,11 +75,23 @@ const Singlepage = () => {
     </div>
   );
 };
+async function getPostById(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  //const post = await res.json(); // обработка json
+  return res.json();
+}
+// получаем комментарии
+async function getCommentsByPost(id) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}/comments`
+  );
+  return res.json();
+}
+
 const postLoader = async ({ params }) => {
   const id = params.id;
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  const post = await res.json(); // обработка json
-  return { post, id };
+  // что-то нужно сразу, другое потом, если добавить await к постам они загрузятся сразу, потом зайдет польз на стр и только тогда загрузятся коменты
+  return { post: await getPostById(id), id, comments: getCommentsByPost(id) };
 };
 
 export { Singlepage, postLoader };
